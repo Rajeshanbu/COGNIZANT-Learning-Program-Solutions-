677414f74692731442d022672f3081cb8f5ed6b4
Rajesh  A <118924713+Rajeshanbu@users.noreply.github.com>
2025-07-16 18:05:27 +0530

    added
    added

---
+++ WEEK_2/Nunit and Moq/moq-demo/2-Exercise/DirectoryExplorer.Tests/Program.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Moq;
+using MagicFilesLib;
+using System.Collections.Generic;
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private Mock<IDirectoryExplorer> _mockExplorer;
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockExplorer = new Mock<IDirectoryExplorer>();
+            _mockExplorer.Setup(x => x.GetFiles(It.IsAny<string>()))
+                         .Returns(new List<string> { _file1, _file2 });
+        }
+
+        [Test]
+        public void Test_GetFiles_ReturnsExpectedFiles()
+        {
+            var files = _mockExplorer.Object.GetFiles("C:\\TestPath");
+
+            Assert.That(files, Is.Not.Null);
+            Assert.That(files.Count, Is.EqualTo(2));
+            Assert.That(files, Does.Contain(_file1));
+        }
+    }
+}
---
+++ WEEK_2/Nunit and Moq/moq-demo/3-Exercise/PlayersManagerLib.Tests/PlayerTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Moq;
+using PlayersManagerLib;
+
+namespace PlayersManagerLib.Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        private Mock<IPlayerMapper> mockMapper;
+        private string _testName;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockMapper = new Mock<IPlayerMapper>();
+            _testName = "Rohit";
+        }
+
+        [Test]
+        public void RegisterNewPlayer_WithValidName_ReturnsPlayer()
+        {
+            mockMapper.Setup(m => m.IsPlayerNameExistsInDb(_testName)).Returns(false);
+
+            Player player = Player.RegisterNewPlayer(_testName, mockMapper.Object);
+
+            Assert.That(player, Is.Not.Null);
+            Assert.That(player.Name, Is.EqualTo(_testName));
+            Assert.That(player.Country, Is.EqualTo("India"));
+        }
+
+        [Test]
+        public void RegisterNewPlayer_WithDuplicateName_ThrowsException()
+        {
+            mockMapper.Setup(m => m.IsPlayerNameExistsInDb(_testName)).Returns(true);
+
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                Player.RegisterNewPlayer(_testName, mockMapper.Object);
+            });
+        }
+    }
+}
